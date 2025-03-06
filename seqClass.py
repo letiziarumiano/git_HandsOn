1616bfb8bde7bd52102fbb192ae9a2fa9fc962b9
letiziarumiano <letizia.rumiano@uvic.cat>
2025-03-06 14:01:56 +0100

    Modify motif not found message in motif branch

--- seqClass.py
+++ seqClass.py
@@ -3,23 +3,32 @@
 import sys, re
 from argparse import ArgumentParser
 
-parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
-parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
+parser = ArgumentParser(description="Classify a sequence as DNA or RNA and search for motifs")
 
-if len(sys.argv) == 1:
-    parser.print_help()
-    sys.exit(1)
+parser.add_argument("-s", "--seq", type=str, required=True, help="Input sequence")
+parser.add_argument("-m", "--motif", type=str, required=False, help="Motif")
 
 args = parser.parse_args()
 
-args.seq = args.seq.upper()  # This line converts the sequence to uppercase
+args.seq = args.seq.upper()
+
 
 if re.search('^[ACGTU]+$', args.seq):
     if re.search('T', args.seq):
-        print ('The sequence is DNA')
+        print('The sequence is DNA')
     elif re.search('U', args.seq):
-        print ('The sequence is RNA')
+        print('The sequence is RNA')
     else:
-        print ('The sequence can be DNA or RNA')
+        print('The sequence can be DNA or RNA')
 else:
-    print ('The sequence is not DNA nor RNA')
+    print('The sequence is not DNA nor RNA')
+
+# If a motif is provided, search for it in the sequence
+if args.motif:
+    args.motif = args.motif.upper()
+    print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end='')
+    if re.search(args.motif, args.seq):
+        print("FOUND")
+    else:
+        print("Motif not found!")
+
